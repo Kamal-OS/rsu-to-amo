77fdb1cccd63224a074788d9821f42c5960b2a4c
KAMAL TAGHLAOUI <102923258+Kamal-OS@users.noreply.github.com>
2025-06-01 22:54:51 +0100

    update links after migrating to the new repo, update demo page

--- popup/popup.js
+++ popup/popup.js
@@ -1,41 +1,38 @@
 // make <a> tags open href url in new tab
-document.addEventListener('DOMContentLoaded', function () {
-    var links = document.getElementsByTagName("a");
-    for (let i = 0; i < links.length; i++) {
-        (function () {
-            var ln = links[i];
-            var location = ln.href;
-            ln.onclick = function () {
-                chrome.tabs.create({active: true, url: location});
-            };
-        })();
-    }
+document.addEventListener("DOMContentLoaded", function () {
+  var links = document.getElementsByTagName("a")
+  for (let i = 0; i < links.length; i++) {
+    ;(function () {
+      var ln = links[i]
+      var location = ln.href
+      ln.onclick = function () {
+        chrome.tabs.create({ active: true, url: location })
+      }
+    })()
+  }
 })
 
-
 // collapsible
-var coll = document.getElementsByClassName("collapsible");
+var coll = document.getElementsByClassName("collapsible")
 for (let i = 0; i < coll.length; i++) {
-  coll[i].addEventListener("click", function() {
-    this.classList.toggle("active");
-    var content = this.nextElementSibling;
-    if (content.style.maxHeight){
-      content.style.maxHeight = null;
+  coll[i].addEventListener("click", function () {
+    this.classList.toggle("active")
+    var content = this.nextElementSibling
+    if (content.style.maxHeight) {
+      content.style.maxHeight = null
     } else {
-      content.style.maxHeight = content.scrollHeight + "px";
-    } 
-  });
+      content.style.maxHeight = content.scrollHeight + "px"
+    }
+  })
 }
-
 
 // demo
 document.querySelector(".demo-btn").addEventListener("click", () => {
-    const demoPage = "https://kamal-os.github.io/RSU-to-AMO/demo/demo.html"
-    chrome.tabs.create({
-        url: demoPage
-    })
+  const demoPage = "https://kamal-os.github.io/rsu-to-amo/demo/demo.html"
+  chrome.tabs.create({
+    url: demoPage,
+  })
 })
-
 
 // parameters
 const inputs = document.querySelectorAll(".parameters input")
@@ -49,10 +46,10 @@ inputs.forEach(async (input) => {
     const target = event.target
     const targetID = target.id.toString()
     const targetState = target.checked
-    
+
     // set current state in storage
     await chrome.storage.local.set({ [targetID]: targetState })
-    
+
     event.preventDefault()
     event.stopPropagation()
   })
--- scripts/content/amo.js
+++ scripts/content/amo.js
@@ -1,12 +1,11 @@
-"use strict";
-
+"use strict"
 
 // Modal popup box
 const htmlPopupBox = `
         <div class="_modal">
             <div class="_popup_box">
                 <h1>رب الأسرة في السجل الاجتماعي الموحد هي الزوجة</h1>
-                <label>ماذا تريدنا أن نفعل (كمال ^_^)</label>
+                <label>اختر بعناية أحد الخيارات الآتية</label>
                 <div class="btns">
                     <button class="btn0 _popup" value="nothing">إغلاق
                         <span class="_popuptext">
@@ -53,204 +52,219 @@ const htmlDownloadAlert = `
     `
 
 chrome.runtime.onMessage.addListener(async (message, sender, sendResponse) => {
-    if (message.type === "amo-form-insert") {
+  if (message.type === "amo-form-insert") {
+    const family = message.data
 
-        const family = message.data
+    if (!family) return
 
-        if (!family) return
-
-        // move spouse to master if master is null
-        if (!family.master) {
-            if (family.spouse) {
-                family.master = family.spouse
-                family.spouse = null
-            }
-            // both null
-            else {
-                return
-            }
-        }
-
-        // family master is female problem
-        if (family.master && family.spouse && family.master.gender === "F" && family.spouse.gender === "M") {
-            await insertPopupBox(family)
-        }
-        
-        //// master
-        const isMarried = family.spouse ? true : false
-        insertMaster(family.master, isMarried)
-
-        //// spouse
-        insertSpouse(family.spouse)
-
-        //// children
-        for (const child of family.children) {
-            await insertChildren(child, sender.id)
-        }
-
-        chrome.runtime.sendMessage(sender.id,
-            {
-                type: "amo-form-done"
-            }
-        )
+    // move spouse to master if master is null
+    if (!family.master) {
+      if (family.spouse) {
+        family.master = family.spouse
+        family.spouse = null
+      }
+      // both null
+      else {
+        return
+      }
     }
 
-    if (message.type === "amo-inject-nextbtn") {
-        const downloadBtn = document.querySelector("main input")
-        if (downloadBtn) {
-            
-            
-            const nextBtn = document.createElement('div')
-            nextBtn.innerHTML = htmlNextBtn
-            downloadBtn.insertAdjacentElement('afterend', nextBtn)
-            
-            nextBtn.querySelector("button").addEventListener("click", () => {
-                chrome.runtime.sendMessage(sender.id,
-                    {
-                        type: "amo-next-family"
-                    }
-                    )
-            })
-            
-            // insert alert message
-            downloadBtn.insertAdjacentHTML('afterend', htmlDownloadAlert)
-        }
+    // family master is female problem
+    if (
+      family.master &&
+      family.spouse &&
+      family.master.gender === "F" &&
+      family.spouse.gender === "M"
+    ) {
+      await insertPopupBox(family)
     }
+
+    //// master
+    const isMarried = family.spouse ? true : false
+    insertMaster(family.master, isMarried)
+
+    //// spouse
+    insertSpouse(family.spouse)
+
+    //// children
+    for (const child of family.children) {
+      await insertChildren(child, sender.id)
+    }
+
+    chrome.runtime.sendMessage(sender.id, {
+      type: "amo-form-done",
+    })
+  }
+
+  if (message.type === "amo-inject-nextbtn") {
+    const downloadBtn = document.querySelector("main input")
+    if (downloadBtn) {
+      const nextBtn = document.createElement("div")
+      nextBtn.innerHTML = htmlNextBtn
+      downloadBtn.insertAdjacentElement("afterend", nextBtn)
+
+      nextBtn.querySelector("button").addEventListener("click", () => {
+        chrome.runtime.sendMessage(sender.id, {
+          type: "amo-next-family",
+        })
+      })
+
+      // insert alert message
+      downloadBtn.insertAdjacentHTML("afterend", htmlDownloadAlert)
+    }
+  }
 })
 
 const insertPopupBox = (family) => {
-    return new Promise(resolve => {
-        document.body.insertAdjacentHTML('beforeend', htmlPopupBox)
-        const popupBox = document.querySelector("._modal")
-        const btns = popupBox.querySelectorAll(".btns button")
-        btns.forEach((btn) => {
-            btn.addEventListener("click", (event) => {
-                const response = event.target.value
+  return new Promise((resolve) => {
+    document.body.insertAdjacentHTML("beforeend", htmlPopupBox)
+    const popupBox = document.querySelector("._modal")
+    const btns = popupBox.querySelectorAll(".btns button")
+    btns.forEach((btn) => {
+      btn.addEventListener("click", (event) => {
+        const response = event.target.value
 
-                switch (response.trim()) {
-                    case "swap-gender": {
-                        family.master.gender = "M"
-                        family.spouse.gender = "F"
-                        break
-                    }
-                    case "swap-master": {
-                        const temp = family.master
-                        family.master = family.spouse
-                        family.spouse = temp
-                        break
-                    }
-                }
+        switch (response.trim()) {
+          case "swap-gender": {
+            family.master.gender = "M"
+            family.spouse.gender = "F"
+            break
+          }
+          case "swap-master": {
+            const temp = family.master
+            family.master = family.spouse
+            family.spouse = temp
+            break
+          }
+        }
 
-                popupBox.style.display = 'none'
-                resolve()
-            })
-        })
+        popupBox.style.display = "none"
+        resolve()
+      })
     })
+  })
 }
 
 const insertMaster = (master, isMarried) => {
+  if (!master) return
 
-    if (!master) return
+  /// get html fields of family master
+  const masterFields = {
+    idcs: document.querySelector("#ctl00_ContentPlaceHolder1_Txt_IDCS"),
+    birthdate: {
+      year: document.querySelector("#Cbo_Annees_Naiss"),
+      month: document.querySelector("#Cbo_Mois_Naiss"),
+      day: document.querySelector("#Cbo_Jours_Naiss"),
+    },
+    gender: document.querySelector("#ctl00_ContentPlaceHolder1_Est_Sexe"),
+    isMarried: document.querySelector(
+      "#ctl00_ContentPlaceHolder1_Est_Existe_Conjoint"
+    ),
+  }
 
-    /// get html fields of family master
-    const masterFields = {
-        idcs: document.querySelector("#ctl00_ContentPlaceHolder1_Txt_IDCS"),
-        birthdate: {
-            year: document.querySelector("#Cbo_Annees_Naiss"),
-            month: document.querySelector("#Cbo_Mois_Naiss"),
-            day: document.querySelector("#Cbo_Jours_Naiss")
-        },
-        gender: document.querySelector("#ctl00_ContentPlaceHolder1_Est_Sexe"),
-        isMarried: document.querySelector("#ctl00_ContentPlaceHolder1_Est_Existe_Conjoint")
-    }
-
-    /// set html fields of family master
-    // idcs
-    masterFields.idcs.value = master.idcs.toString()
-    // brithdate
-    masterFields.birthdate.year.value = master.birthdate.year.toString()
-    masterFields.birthdate.month.value = Number(master.birthdate.month).toLocaleString('en-US', { minimumIntegerDigits: 2, useGrouping: false })
-    masterFields.birthdate.day.value = Number(master.birthdate.day).toLocaleString('en-US', { minimumIntegerDigits: 2, useGrouping: false })
-    // gender (default to male)
-    masterFields.gender.querySelector(`input[value=${master.gender}]`).click()
-    // is married?
-    masterFields.isMarried.querySelector(`input[value="${isMarried ? 1 : 0}"]`).click()
+  /// set html fields of family master
+  // idcs
+  masterFields.idcs.value = master.idcs.toString()
+  // brithdate
+  masterFields.birthdate.year.value = master.birthdate.year.toString()
+  masterFields.birthdate.month.value = Number(
+    master.birthdate.month
+  ).toLocaleString("en-US", { minimumIntegerDigits: 2, useGrouping: false })
+  masterFields.birthdate.day.value = Number(
+    master.birthdate.day
+  ).toLocaleString("en-US", { minimumIntegerDigits: 2, useGrouping: false })
+  // gender (default to male)
+  masterFields.gender.querySelector(`input[value=${master.gender}]`).click()
+  // is married?
+  masterFields.isMarried
+    .querySelector(`input[value="${isMarried ? 1 : 0}"]`)
+    .click()
 }
 
 const insertSpouse = (spouse) => {
+  if (!spouse) return
 
-    if (!spouse) return
+  // wait until spouse fileds are loaded then set them
+  const tempInterval = setInterval(() => {
+    /// get spouse fields
+    const spouseFields = {
+      idcs: document.querySelector(
+        "#ctl00_ContentPlaceHolder1_Txt_IDCS_Conjoint"
+      ),
+      birthdate: {
+        year: document.querySelector("#Cbo_Annees_Naiss_Conjoint"),
+        month: document.querySelector("#Cbo_Mois_Naiss_Conjoint"),
+        day: document.querySelector("#Cbo_Jours_Naiss_Conjoint"),
+      },
+      gender: document.querySelector(
+        "#ctl00_ContentPlaceHolder1_Est_Sexe_Conjoint"
+      ),
+    }
 
-    // wait until spouse fileds are loaded then set them
-    const tempInterval = setInterval(() => {
-        /// get spouse fields
-        const spouseFields = {
-            idcs: document.querySelector("#ctl00_ContentPlaceHolder1_Txt_IDCS_Conjoint"),
-            birthdate: {
-                year: document.querySelector("#Cbo_Annees_Naiss_Conjoint"),
-                month: document.querySelector("#Cbo_Mois_Naiss_Conjoint"),
-                day: document.querySelector("#Cbo_Jours_Naiss_Conjoint")
-            },
-            gender: document.querySelector("#ctl00_ContentPlaceHolder1_Est_Sexe_Conjoint"),
-        }
+    // check
+    if (!spouseFields.idcs || spouseFields.idcs.value) return
 
-        // check
-        if (!spouseFields.idcs || spouseFields.idcs.value) return
+    /// set spouse fields
+    // idcs
+    spouseFields.idcs.value = spouse.idcs.toString()
+    // brithdate
+    spouseFields.birthdate.year.value = spouse.birthdate.year
+    spouseFields.birthdate.month.value = Number(
+      spouse.birthdate.month
+    ).toLocaleString("en-US", { minimumIntegerDigits: 2, useGrouping: false })
+    spouseFields.birthdate.day.value = Number(
+      spouse.birthdate.day
+    ).toLocaleString("en-US", { minimumIntegerDigits: 2, useGrouping: false })
+    // gender (default to female)
+    spouseFields.gender.querySelector(`input[value="${spouse.gender}"]`).click()
 
-        /// set spouse fields
-        // idcs
-        spouseFields.idcs.value = spouse.idcs.toString()
-        // brithdate
-        spouseFields.birthdate.year.value = spouse.birthdate.year
-        spouseFields.birthdate.month.value = Number(spouse.birthdate.month).toLocaleString('en-US', { minimumIntegerDigits: 2, useGrouping: false })
-        spouseFields.birthdate.day.value = Number(spouse.birthdate.day).toLocaleString('en-US', { minimumIntegerDigits: 2, useGrouping: false })
-        // gender (default to female)
-        spouseFields.gender.querySelector(`input[value="${spouse.gender}"]`).click()
-
-        // stop
-        clearInterval(tempInterval)
-    }, 500)
+    // stop
+    clearInterval(tempInterval)
+  }, 500)
 }
 
 const insertChildren = async (child, tabId) => {
+  if (!child) return
 
-    if (!child) return
+  return new Promise((resolve) => {
+    // wait until child fileds are loaded (after add btn click) then set them
+    const tempInterval = setInterval(() => {
+      /// get child fields
+      const childFields = {
+        idcs: document.querySelector('[id*="Txt_IDCS_Enfant_Ajt"]'),
+        birthdate: {
+          year: document.querySelector("#Cbo_Annees_Naiss_Enfant_Ajt"),
+          month: document.querySelector("#Cbo_Mois_Naiss_Enfant_Ajt"),
+          day: document.querySelector("#Cbo_Jours_Naiss_Enfant_Ajt"),
+        },
+        gender: document.querySelector('[id*="C_Sexe_Enfant_Ajt"'),
+      }
 
-    return new Promise((resolve) => {
-        // wait until child fileds are loaded (after add btn click) then set them
-        const tempInterval = setInterval(() => {
-            /// get child fields
-            const childFields = {
-                idcs: document.querySelector('[id*="Txt_IDCS_Enfant_Ajt"]'),
-                birthdate: {
-                    year: document.querySelector("#Cbo_Annees_Naiss_Enfant_Ajt"),
-                    month: document.querySelector("#Cbo_Mois_Naiss_Enfant_Ajt"),
-                    day: document.querySelector("#Cbo_Jours_Naiss_Enfant_Ajt")
-                },
-                gender: document.querySelector('[id*="C_Sexe_Enfant_Ajt"'),
-            }
+      // checking
+      if (!childFields.idcs || childFields.idcs.value) return
 
-            // checking
-            if (!childFields.idcs || childFields.idcs.value) return
+      /// set child fields
+      // idcs
+      childFields.idcs.value = child.idcs.toString()
+      // brithdate
+      childFields.birthdate.year.value = child.birthdate.year
+      childFields.birthdate.month.value = Number(
+        child.birthdate.month
+      ).toLocaleString("en-US", { minimumIntegerDigits: 2, useGrouping: false })
+      childFields.birthdate.day.value = Number(
+        child.birthdate.day
+      ).toLocaleString("en-US", { minimumIntegerDigits: 2, useGrouping: false })
+      // gender
+      childFields.gender.querySelector(`input[value="${child.gender}"]`).click()
 
-            /// set child fields
-            // idcs
-            childFields.idcs.value = child.idcs.toString()
-            // brithdate
-            childFields.birthdate.year.value = child.birthdate.year
-            childFields.birthdate.month.value = Number(child.birthdate.month).toLocaleString('en-US', { minimumIntegerDigits: 2, useGrouping: false })
-            childFields.birthdate.day.value = Number(child.birthdate.day).toLocaleString('en-US', { minimumIntegerDigits: 2, useGrouping: false })
-            // gender
-            childFields.gender.querySelector(`input[value="${child.gender}"]`).click()
+      /// fields are ready just click the add button
+      chrome.runtime.sendMessage(tabId, {
+        type: "click-add-child",
+      })
 
-            /// fields are ready just click the add button
-            chrome.runtime.sendMessage(tabId, {
-                type: "click-add-child"
-            })
-
-            // stop
-            clearInterval(tempInterval)
-            resolve()
-        }, 500)
-    })
+      // stop
+      clearInterval(tempInterval)
+      resolve()
+    }, 500)
+  })
 }
